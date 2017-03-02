80a539bef37c8f219c00f4731995418c771e0104
Julià <jcolomr18@gmail.com>
2017-03-02 16:44:50 +0100

    acabat amb comentaris fets

--- Desktop/clase/grau superio/programacio/AssciArt/src/Printer.java
+++ Desktop/clase/grau superio/programacio/AssciArt/src/Printer.java
@@ -13,15 +13,19 @@ public class Printer {
     // Hi haurà 27 lletres en total, separades per un space en blanc.
     Printer(String alphabet) {
         this.alphabet = alphabet;
+        //calculam la altura de cada lletre
         this.AlturaLletra = alphabet.length() / 27 - 14;
+        //calculam el llarc de cada lletre
         this.LlarcLletre = (alphabet.length() / 27) / this.AlturaLletra;
+        //calculam cuan em de canviar la lletra
         this.CanviaLletra = alphabet.length() / AlturaLletra + 1;
-
+        //cram els espais que hi ha entre paraula i paraula
         if (this.AlturaLletra == 5) {
             this.Espai = "    ";
         } else {
             this.Espai = "      ";
         }
+        //volem sebre onesta la posicio
         this.SebrePos = CanviaLletra - Espai.length();
     }
 
@@ -31,28 +35,34 @@ public class Printer {
     // minúscules es passen a majúscules, i els altres caràctes es tradueixen
     // a «?» (manco l'space en blanc).
     public String render(String text) {
+        //guardam la lletra o lletres que ens pasen
         char[] Lletres = text.toUpperCase().toCharArray();
         String Result = "";
+        //Feim un bucle per fer la altura de la lletra
         for (int i = 0; i < this.AlturaLletra; i++) {
+            //Feim un bucle per construir cada linea
             for (int j = 0; j < Lletres.length; j++) {
+                //comprovam si hi ha espais entre paraules
                 if (Lletres[j] == ' ') {
                     Result += this.Espai;
                     continue;
                 }
+                //calculam la pasicio on comensam a escriure
                 int Pos = i * this.CanviaLletra;
+                //comprovam si esta entre la A i la Z si nos posam caracter ?
                 if (Lletres[j] < 'A' || Lletres[j] > 'Z') {
                     Pos += this.SebrePos;
                 } else {
                     Pos += this.LlarcLletre * (Lletres[j] - 'A') + (Lletres[j] - 'A');
                 }
-
+                //comensam a escriure la lletra
                 Result += this.alphabet.substring(Pos, Pos + this.LlarcLletre);
+                //posam l/espai entre lletra i lletra
                 if (Lletres.length > 1 && j < Lletres.length - 1) {
                     Result += " ";
                 }
             }
-
-
+            //comprovam si no es la darrera linea i si no ho es li posam un \n
             if (i < this.AlturaLletra - 1) {
                 Result += "\n";
             }
